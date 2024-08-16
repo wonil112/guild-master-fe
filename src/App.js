83d215ffc87381150caeebf31ae5c3c647e91d91
wonil112 <nicewonil@naver.com>
2024-08-16 18:38:35 +0900

    add test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { useState } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import GuildListPage from './pages/GuildListPage';
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/guildlist" element={<GuildListPage />} />
-        </Routes>
-      </div>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/guildlist" element={<GuildListPage />} />
+            <Route path="/home" element={<HomePage />} />
+          </Routes>
+        </div>
     </Router>
   );
 }
