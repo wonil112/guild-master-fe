5da6c403db98270981ad9fc906bd68dba2349320
skyla00 <hanulofu@gmail.com>
2024-08-17 20:27:09 +0900

    add data

---
+++ src/data/guildData.js
@@ -0,0 +1,11 @@
+export const guildData = [
+    {guildId: 1, gameId: 1, guildName : 'Guild name A', guildCurrentPopulation:'12', guildTotalPopulation:'20'},
+    {guildId: 2, gameId: 1, guildName : 'Guild name B', guildCurrentPopulation:'30', guildTotalPopulation:'50'},
+    {guildId: 3, gameId: 2, guildName : 'Guild name C', guildCurrentPopulation:'145', guildTotalPopulation:'200'},
+    {guildId: 4, gameId: 2, guildName : 'Guild name D', guildCurrentPopulation:'200', guildTotalPopulation:'300'},
+    {guildId: 5, gameId: 2, guildName : 'Guild name E', guildCurrentPopulation:'70', guildTotalPopulation:'250'},
+    {guildId: 6, gameId: 3, guildName : 'Guild name F', guildCurrentPopulation:'123', guildTotalPopulation:'300'},
+    {guildId: 7, gameId: 3, guildName : 'Guild name G', guildCurrentPopulation:'345', guildTotalPopulation:'430'},
+    {guildId: 8, gameId: 4, guildName : 'Guild name H', guildCurrentPopulation:'10', guildTotalPopulation:'90'}
+    
+  ];
